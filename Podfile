628263f11b72f6ae15109c502be5d313edee06c4
Sean Allen <allen.sean.t@gmail.com>
2016-06-22 23:14:53 -0700

    Callback URL Pod installed

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'Rev11 Scan' do
+  use_frameworks!
+  pod 'CallbackURLKit'
+
+end
---
+++ Pods/Target Support Files/Pods-Rev11 Scan/Pods-Rev11 Scan-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Rev11_ScanVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Rev11_ScanVersionString[];
+
