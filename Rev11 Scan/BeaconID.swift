b098427cc8b382eb7d855f0d816643a1a58e8c1a
Sean Allen <allen.sean.t@gmail.com>
2016-09-05 18:25:25 -0700

    committing before messing with bridging header

--- Rev11 Scan/BeaconID.swift
+++ Rev11 Scan/BeaconID.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation 
 
 struct BeaconID: Equatable, CustomStringConvertible, Hashable {
 
--- Rev11Scan-ShareExtension/ShareViewController.swift
+++ Rev11Scan-ShareExtension/ShareViewController.swift
@@ -13,69 +13,58 @@ import MobileCoreServices
 class ShareViewController: SLComposeServiceViewController {
 
   let suiteName = "group.deegeu.swift.share.extension"
-  let redDefaultKey = "RedColorImage"
-  let blueDefaultKey = "BlueColorImage"
 
   var selectedImage: UIImage?
-  var selectedColorName = "Default"
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+  override func isContentValid() -> Bool {
+      return true
     }
 
-    override func didSelectPost() {
-      // This is called after the user selects Post.
-      // Make sure we have a valid extension item
-      if let content = extensionContext!.inputItems[0] as? NSExtensionItem {
-        let contentType = kUTTypeImage as String
-        // A couple kUTTypes that might work... kUTTypeJSON, kUTTypeCommaSeparatedText, kUTTypeSpreadsheet, kUTTypeFileURL
+  override func didSelectPost() {
+    // This is called after the user selects Post.
+    // Make sure we have a valid extension item
+    if let content = extensionContext!.inputItems[0] as? NSExtensionItem {
+//       let contentType = kUTTypeImage as String
+      let contentType = kUTTypeSpreadsheet as String
+      // A couple kUTTypes that might work... kUTTypeJSON, kUTTypeCommaSeparatedText, kUTTypeSpreadsheet, kUTTypeFileURL
 
-        // Verify the provider is valid
-        if let contents = content.attachments as? [NSItemProvider] {
+      // Verify the provider is valid
+      if let contents = content.attachments as? [NSItemProvider] {
 
-          // look for images
-          for attachment in contents {
-            if attachment.hasItemConformingToTypeIdentifier(contentType) {
-              attachment.loadItem(forTypeIdentifier: contentType, options: nil) { data, error in
+        // look for images
+        for attachment in contents {
 
-                let url = data as! NSURL
-                if (!self.selectedColorName.isEmpty) {
-                  if let imageData = NSData(contentsOf: url as URL) {
-                    if (self.selectedColorName == "Default") {
-                      self.saveImage(color: self.redDefaultKey, imageData: imageData)
-                      self.saveImage(color: self.blueDefaultKey, imageData: imageData)
-                    } else if (self.selectedColorName == "Red") {
-                      self.saveImage(color: self.redDefaultKey, imageData: imageData)
-                    } else {
-                      // must be blue
-                      self.saveImage(color: self.blueDefaultKey, imageData: imageData)
-                    }
-                  }
-                }
+          if attachment.hasItemConformingToTypeIdentifier(contentType) {
+            attachment.loadItem(forTypeIdentifier: contentType, options: nil) { data, error in
+
+              let url = data as! NSURL
+
+              if let spreadsheetData = NSData(contentsOf: url as URL) {
+                self.saveSpreadsheet(data: spreadsheetData)
               }
             }
           }
         }
       }
-
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
+      // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+      self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+  }
 
 // Need to convert "saveImage" to "saveFile" for .csv or .json.
 
-  // Saves an image to user defaults.
-  func saveImage(color: String, imageData: NSData) {
-    if let prefs = UserDefaults(suiteName: suiteName) {
-      prefs.removeObject(forKey: color)
-      prefs.set(imageData, forKey: color)
-    }
+  func saveSpreadsheet(data: NSData) {
+    let prefs = UserDefaults.standard
+    prefs.set(data, forKey: "spreadsheetFileAsData")
   }
+
+  // Saves an image to user defaults.
+//  func saveImage(color: String, imageData: NSData) {
+//    if let prefs = UserDefaults(suiteName: suiteName) {
+//      prefs.removeObject(forKey: color)
+//      prefs.set(imageData, forKey: color)
+//    }
+//  }
 
 }
